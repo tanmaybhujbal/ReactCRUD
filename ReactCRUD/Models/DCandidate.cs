cfd7b89aa051a4d7ecc0ac13c39bd56cb4c39f1e
Tanmay Bhujbal <dev.tanmayb@gmail.com>
2020-07-13 20:41:48 +0530

    Code commit

---
+++ ReactCRUD/Models/DCandidate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactCRUD.Models
+{
+    public class DCandidate
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string fullName { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string mobile { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string email { get; set; }
+
+        public int age { get; set; }
+
+        [Column(TypeName = "nvarchar(3)")]
+        public string bloodgroup { get; set; }
+
+        [Column(TypeName = "nvarchar(200)")]
+        public string address{ get; set; }
+    }
+}
